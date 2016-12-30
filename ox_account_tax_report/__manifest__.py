ac8a4b679d7bff95ad5c59b57043d9f617f8bc87
jaquinonesg <jaquinonesg@unal.edu.co>
2016-12-30 08:07:26 -0500

    nueva versión de los informes para impuestos. El cual permite agregar a cada impuesto varios grupos de impuestos, y así poder generar el formato 300 para la dian

---
+++ ox_account_tax_report/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Informes para Impuestos",
+
+    'summary': """Informes configurables para declaración de impuestos""",
+
+    'description': """
+        Este modulo permite configurar informes por categorías de impuestos y cuentas contables
+    """,
+
+    'author': "OxSoft SAS",
+    'website': "http://www.oxsoft.com.co",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Accounting',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['account'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'account_tax_report_view.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        #'demo.xml',
+    ],
+}
