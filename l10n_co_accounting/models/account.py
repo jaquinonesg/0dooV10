4283a52fc4276e88089cc6fd9efecba6d7a0476e
Jorge Quinones <jaquinonesg@gmail.com>
2017-02-08 15:40:17 -0500

    Agregado l10n_co_accounting

---
+++ l10n_co_accounting/models/account.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class account(models.Model):
+    _inherit = 'account.account'
+
+    parent_id = fields.Many2one('account.account', string='Parent ID')
+
+class contabilidad(models.Model):
+    _inherit = 'account.account.template'
+
+    parent_id = fields.Many2one('account.account', string='Parent ID')
