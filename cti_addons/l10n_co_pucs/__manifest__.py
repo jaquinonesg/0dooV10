f63512c9e3ed53ac3c8a866351fab0cfafaf430b
jaquinonesg <jaquinonesg@unal.edu.co>
2017-02-13 16:32:43 -0500

    Actualizado modulo l10n_co_pucs

--- cti_addons/l10n_co_pucs/__manifest__.py
+++ cti_addons/l10n_co_pucs/__manifest__.py
@@ -30,8 +30,8 @@
         #'data/post.account.chart.template.xml',
         'data/account.account.template.csv',
         'data/account.tax.template.csv',
-        'data/account.fiscal.position.template.csv',
-        'data/account.fiscal.position.tax.template.csv',
+        #'data/account.fiscal.position.template.csv',
+        #'data/account.fiscal.position.tax.template.csv',
         #'data/account.account.template.parent.id.xml',
     ],
     # only loaded in demonstration mode
