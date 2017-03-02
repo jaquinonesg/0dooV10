ff5e2c11d5cd0f25b910d2864419971fcd571a9d
jaquinonesg <jaquinonesg@unal.edu.co>
2017-03-02 11:01:10 -0500

    Cambios en ox_cost_manufacture_finished

--- cti_addons/ox_cost_manufacture_finished/cost_manufacture.py
+++ cti_addons/ox_cost_manufacture_finished/cost_manufacture.py
@@ -3,6 +3,7 @@
 from odoo import models, fields, api
 from odoo.exceptions import UserError
 import logging
+import math
 
 _logger = logging.getLogger(__name__)
 
@@ -30,6 +31,31 @@ class StockMoveQualityControl(models.Model):
 
     inspection_id = fields.Many2one('qc.inspection',string='QC Inspection')
 
+class MRPRoutingWorkCenterCostExpected(models.Model):
+    _inherit = 'mrp.routing.workcenter'
+
+    @api.depends('default_cost','default_cost_per_labor')
+    def _get_total(self):
+        self.total_cost=self.default_cost+self.default_cost_per_labor
+
+    default_cost = fields.Float(string='Default cost')
+    default_cost_per_labor = fields.Float(string='Default cost per labor')
+    total_cost = fields.Float(compute='_get_total', string='Total cost',store=False)
+
+class MRPWorkorderCost(models.Model):
+    _inherit = 'mrp.workorder'
+
+    @api.depends('real_cost','real_cost_per_labor','duration')
+    def _get_total(self):
+        for x in self:
+            x.total_cost=(x.real_cost+x.real_cost_per_labor)*x.duration
+
+    default_cost = fields.Float(string='Default cost')
+    default_cost_per_labor = fields.Float(string='Default cost per labor')
+    real_cost = fields.Float(string='Real cost',required=True,default=0)
+    real_cost_per_labor = fields.Float(string='Real cost per labor',required=True,default=0)
+    total_cost = fields.Float(compute='_get_total', string='Total cost',store=False)
+
 class CostManufacture(models.Model):
     _inherit = 'mrp.production'
 
@@ -44,10 +70,73 @@ class CostManufacture(models.Model):
     gr_sl = fields.Float(string='GR S-L', default=0.0)
     standard_price = fields.Float(string='Cost', default=0.0)
 
-    def open_produce_product(self):
-        self.CalculateCost()
-        return super(CostManufacture, self).open_produce_product()
+    # @api.multi
+    # def open_produce_product(self):
+        
+    #     return super(CostManufacture, self).open_produce_product()
 
+    @api.multi
+    def button_mark_done(self):
+        _logger.info(self)
+        self.CalculateCost()
+        self.ensure_one()
+        for wo in self.workorder_ids:
+            if wo.time_ids.filtered(lambda x: (not x.date_end) and (x.loss_type in ('productive', 'performance'))):
+                raise UserError(_('Work order %s is still running') % wo.name)
+        self.post_inventory()
+        moves_to_cancel = (self.move_raw_ids | self.move_finished_ids).filtered(lambda x: x.state not in ('done', 'cancel'))
+        moves_to_cancel.action_cancel()
+        self.write({'state': 'done', 'date_finished': fields.Datetime.now()})
+        self.env["procurement.order"].search([('production_id', 'in', self.ids)]).check()
+        self.write({'state': 'done'})
+
+    def _workorders_create(self, bom, bom_data):
+        """
+        :param bom: in case of recursive boms: we could create work orders for child
+                    BoMs
+        """
+        workorders = self.env['mrp.workorder']
+        bom_qty = bom_data['qty']
+
+        # Initial qty producing
+        if self.product_id.tracking == 'serial':
+            quantity = 1.0
+        else:
+            quantity = self.product_qty - sum(self.move_finished_ids.mapped('quantity_done'))
+            quantity = quantity if (quantity > 0) else 0
+
+        for operation in bom.routing_id.operation_ids:
+            # create workorder
+            cycle_number = math.ceil(bom_qty / operation.workcenter_id.capacity)  # TODO: float_round UP
+            duration_expected = (operation.workcenter_id.time_start +
+                                 operation.workcenter_id.time_stop +
+                                 cycle_number * operation.time_cycle * 100.0 / operation.workcenter_id.time_efficiency)
+            workorder = workorders.create({
+                'name': operation.name,
+                'production_id': self.id,
+                'workcenter_id': operation.workcenter_id.id,
+                'operation_id': operation.id,
+                'duration_expected': duration_expected,
+                'state': len(workorders) == 0 and 'ready' or 'pending',
+                'qty_producing': quantity,
+                'default_cost': operation.default_cost,
+                'default_cost_per_labor':operation.default_cost_per_labor,
+                'capacity': operation.workcenter_id.capacity,
+            })
+            if workorders:
+                workorders[-1].next_work_order_id = workorder.id
+            workorders += workorder
+
+            # assign moves; last operation receive all unassigned moves (which case ?)
+            moves_raw = self.move_raw_ids.filtered(lambda move: move.operation_id == operation)
+            if len(workorders) == len(bom.routing_id.operation_ids):
+                moves_raw |= self.move_raw_ids.filtered(lambda move: not move.operation_id)
+            moves_finished = self.move_finished_ids.filtered(lambda move: move.operation_id == operation) #TODO: code does nothing, unless maybe by_products?
+            moves_raw.mapped('move_lot_ids').write({'workorder_id': workorder.id})
+            (moves_finished + moves_raw).write({'workorder_id': workorder.id})
+
+            workorder._generate_lot_ids()
+        return workorders
 
     def CalculateCost(self):
         if self.product_id.cost_special:
@@ -93,20 +182,15 @@ class CostManufacture(models.Model):
                     self.product_id.standard_price = self.gr_sl * self.ov1
                     cost = 0
                     for bl in self.bom_id.bom_line_ids:
-                        _logger.info("Product: %s - Cost %s "%(bl.product_id,bl.product_id.standard_price))
                         if bl.product_id.standard_price:
                             cost += bl.product_id.standard_price
                     fats = 0
                     for sp in self.bom_id.sub_products:
-                        _logger.info("sub-producto: %s "%sp)
                         for att in sp.product_id.attribute_value_ids:
                             if att.attribute_id.fat:
                                 fat = float(att.name.replace(',','.'))/100
                                 fats += fat
                         sp.product_id.standard_price = (cost / (self.st/100))  * fat
-                        _logger.info("Cost: %s "%cost)
-                        _logger.info("fat: %s "%fat)
-                        _logger.info("sub-producto standard price: %s "%sp.product_id.standard_price)
                     qty = abs((self.product_qty * (fat_general - fatc))/(fats*100-fatc))
                     for fi in self.move_finished_ids:
                         fi.price_unit = fi.product_id.standard_price
@@ -126,14 +210,22 @@ class CostManufacture(models.Model):
                 if fi.product_uom_qty>0:
                     cost += fi.price_unit*fi.product_uom_qty
                     cant += fi.product_uom_qty
+            cost_add = 0
+            for ca in self.workorder_ids:
+                cost_add += ca.total_cost
             #promedio
-            cost += self.product_id.standard_price* self.product_id.qty_available
-            cant += self.product_id.qty_available
-            cost = cost / cant
+            cost = self.product_id.standard_price* self.product_id.qty_available
+            cant = self.product_id.qty_available
+            _logger.info('cant')
+            _logger.info(self.product_id.name)
+            _logger.info(cant)
+            cost = cost / (cant or 1)
             for pf in self.move_finished_ids:
                 cant_t += pf.product_uom_qty
             for pf in self.move_finished_ids:
-                new_cost = cost * (pf.product_uom_qty/cant_t)
+                _logger.info('cant_t')
+                _logger.info(cant_t)
+                new_cost = cost * (pf.product_uom_qty/cant_t) + cost_add
                 pf.product_id.standard_price = new_cost
                 pf.price_unit = new_cost
         return True
